5e1800f5693bf496c40e863371cabc7e8026296b
C1manyd <1225249223@qq.com>
2020-12-02 00:42:36 +0800

    Upload

---
+++ main.js
@@ -0,0 +1,215 @@
+var isclick = 0;
+var setting = document.getElementsByClassName("setting-interface");
+var xhr;
+
+window.onload = () => {
+
+    if (window.XMLHttpRequest) {
+        //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+        xhr = new XMLHttpRequest();
+    }
+    else {
+        // IE6, IE5 浏览器执行代码
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    Update()
+}
+
+function start() {
+    xhr.open('get', 'http://127.0.0.1:8000/start')
+    xhr.send()
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (xhr.responseText.search("警告") != -1)
+                    Notice(xhr.responseText, "red")
+                else {
+                    Notice(xhr.responseText, "green")
+                    setTimeout(() => {
+                        Update()
+                    }, 50);
+                }
+            }
+        }
+    }
+}
+
+function clearall() {
+    xhr.open('get', 'http://127.0.0.1:8000/clear')
+    xhr.send()
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (xhr.responseText.search("警告") != -1)
+                    Notice(xhr.responseText, "red")
+                else {
+                    Notice(xhr.responseText, "green")
+                    setTimeout(() => {
+                        Update()
+                    }, 50);
+                }
+            }
+        }
+    }
+}
+
+function Setting_btn() {
+    if (isclick) {
+        // setting[0].style.display="none"
+        var opacity_ = 1
+        var id = setInterval(setnone, 20)
+        function setnone() {
+            if (opacity_ <= 0) {
+                clearInterval(id)
+                setting[0].style.display = "none"
+            }
+            else {
+                setting[0].style.opacity = opacity_
+                opacity_ -= 0.1
+            }
+        }
+        isclick = 0
+    }
+    else {
+        var opacity_ = 0
+        var id = setInterval(setdisplay, 20)
+        function setdisplay() {
+            if (opacity_ > 1) {
+                clearInterval(id)
+            }
+            else {
+                setting[0].style.display = "inline-block"
+                setting[0].style.opacity = opacity_
+                opacity_ += 0.1
+            }
+        }
+        isclick = 1
+    }
+}
+
+function Award_S() {
+    var content = document.getElementById('award').value
+    var chance = document.getElementById('chance').value
+    var tag = document.getElementById('tag').value
+    xhr.open('get', 'http://127.0.0.1:8000/award?content=' + content + '&chance=' + chance + '&tag=' + tag)
+    xhr.send()
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (xhr.responseText.search("警告") != -1)
+                    Notice(xhr.responseText, "red")
+                else {
+                    Notice(xhr.responseText, "green")
+                    setTimeout(() => {
+                        Update()
+                    }, 50);
+                }
+            }
+        }
+    }
+
+}
+
+function Del(e) {
+    var type = e.id.split("-")
+    xhr.open('get', 'http://127.0.0.1:8000/del?id=' + type[1] + '&type=' + type[0])
+    xhr.send()
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (xhr.responseText.search("警告") != -1)
+                    Notice(xhr.responseText, "red")
+                else {
+                    Notice(xhr.responseText, "green")
+                    setTimeout(() => {
+                        Update()
+                    }, 50);
+                }
+            }
+        }
+    }
+}
+
+function Content_S() {
+    var people = document.getElementById('content').value
+    xhr.open('get', 'http://127.0.0.1:8000/content?people=' + people+'&ispicked='+'')
+    xhr.send()
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (xhr.responseText.search("警告") != -1)
+                    Notice(xhr.responseText, "red")
+
+                else {
+                    Notice(xhr.responseText, "green")
+                    Update()
+                }
+
+            }
+        }
+    }
+}
+// //测试，未启用
+// var iTime
+
+// function Notice_bak(text, color) {
+//     clearTimeout(iTime);
+//     iTime = setTimeout(() => {
+//         var opacity_ = 0
+//         var Ctext = document.getElementsByClassName('text')[0]
+//         Ctext.innerHTML = text
+//         Ctext.style.color = color
+//         var id = setInterval(() => {
+//             opacity_ += 0.08
+//             document.getElementsByClassName('notice')[0].style.opacity = opacity_
+//             if (opacity_ > 0.8)
+//                 clearInterval(id)
+//         }, 10)
+//         setTimeout(() => {
+//             var id2 = setInterval(() => {
+//                 opacity_ -= 0.1
+//                 document.getElementsByClassName('notice')[0].style.opacity = opacity_
+//                 if (opacity_ <= 0)
+//                     clearInterval(id2)
+//             }, 15)
+//         }, 1300);
+//     }, 200);
+// }//测试，未启用
+
+function Notice(text, color) {
+    var opacity_ = 0
+    var Ctext = document.getElementsByClassName('text')[0]
+    Ctext.innerHTML = text
+    Ctext.style.color = color
+    var id = setInterval(() => {
+        opacity_ += 0.08
+        document.getElementsByClassName('notice')[0].style.opacity = opacity_
+        if (opacity_ > 0.8)
+            clearInterval(id)
+    }, 10)
+        setTimeout(() => {
+            var id2 = setInterval(() => {
+                opacity_ -= 0.1
+                document.getElementsByClassName('notice')[0].style.opacity = opacity_
+                if (opacity_ <= 0)
+                    clearInterval(id2)
+            }, 15)
+        }, 1300);
+}
+
+function Update() {
+    var content
+    xhr.open('get', 'http://127.0.0.1:8000/update')
+    xhr.send()
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                content = xhr.responseText
+                content = content.split("@")
+                document.getElementById('left-table').innerHTML = content[0]
+                document.getElementById('right-table').innerHTML = content[1]
+            }
+        }
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,161 @@
+// 引入express
+const express = require("express")
+
+// 创建应用对象
+const app = express()
+
+const MAXNUM = 10
+
+var award = []
+
+var content = []
+
+var count_award = 0, count_content = 0
+
+app.get('/award', (req, res) => {
+    var chance_total = 0
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // 设置响应
+    if (count_award == MAXNUM)
+        res.send("警告<br>(已达到最大限制)")
+    award[count_award] = req.query
+    if (award[count_award].content == "" || award[count_award].chance == "")
+        res.send("警告<br>(缺少内容或概率)")
+    else if (award[count_award].chance >= 100)
+        res.send("警告<br>(概率不能等于或超过1)")
+    else if ((function () {
+        if (award[0].content == "")
+            return 'false'
+        for (var i = 0; i < award.length; i++)
+            chance_total += parseFloat(award[i].chance)
+        console.log(chance_total)
+        if (chance_total > 100)
+            return 'true'
+        else
+            return 'false'
+    })() == 'true') {
+        res.send("警告<br>(概率总和超过一)")
+    }
+    else {
+        res.send("提示<br>(添加成功)")
+        console.log(award[count_award])
+        count_award++
+    }
+
+})
+
+app.get('/content', (req, res) => {
+    // 设置响应
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (count_content == MAXNUM)
+        res.send("警告<br>(已达到最大限制)")
+    content[count_content] = req.query
+    if (content[count_content].people == "")
+        res.send("警告<br>(参与者不能为空)")
+    else {
+        res.send("提示<br>(添加成功)")
+        console.log(content[count_content])
+        count_content++
+
+    }
+
+})
+
+app.get('/clear', (req, res) => {
+    // 设置响应
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    award = []
+    content = []
+    count_award = 0
+    count_content = 0
+    res.send("提示<br>(清除成功)")
+})
+
+app.get('/start', (req, res) => {
+    // 设置响应
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (count_content != count_award)
+        res.send("警告<br>(参与者与奖品数量不匹配)")
+    else if (count_award == 0 || count_content == 0)
+        res.send("警告<br>(参与者或奖品为空)")
+    else {
+        var localaward = JSON.parse(JSON.stringify(award))
+        for (var i = 0; i < count_content; i++) {
+            var picked = getResult(localaward)
+            if(picked!=-1)
+                content[i]['ispicked'] = localaward[picked]['content']
+            else
+                content[i]['ispicked']="未中奖"
+            for (var j = picked; j < localaward.length - 1; j++)//去掉已经抽到的奖品
+                localaward[j] = localaward[j + 1]
+            localaward.length--
+            console.log(localaward.length)
+        }
+        res.send("提示<br>(抽签结束)")
+    }
+})
+
+function getResult(arr_origin) {//根据概率抽取随机数
+    var leng = 100;
+    var arr=JSON.parse(JSON.stringify(arr_origin))
+    console.log(arr)
+    for (var i = 0; i < arr.length; i++) {
+        var random_ = parseInt(Math.random() * leng);
+        if (random_ < arr[i]['chance']) {
+            return i
+        }
+        else {
+            leng -= arr[i]['chance']
+        }
+    }
+    return -1
+}
+
+app.get('/update', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var send_content = "<tr><th>参与者</th><th>中奖</th></tr>"
+    for (var i = 0; i < count_content; i++) {
+        send_content += '<tr> <th><i id="people-' + i + '" onclick="Del(this)" title="删除">x</i>' + content[i].people + '</th><th>' + content[i].ispicked + '</th></tr>'
+    }
+    send_content += "@<tr><th>内容</th><th>概率</th><th>标签</th></tr>"
+    for (var i = 0; i < count_award; i++) {
+        send_content += '<tr> <th><i id="award-' + i + '" onclick="Del(this)" title="删除">x</i>' + award[i]['content'] + '</th><th>' + award[i].chance + '</th><th>' + award[i].tag + '</th></tr>'
+    }
+    res.send(send_content)
+})
+
+app.get('/del', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.query.type == "people") {
+        for (var i = parseInt(req.query.id); i < count_content - 1; i++)
+            content[i] = content[i + 1]
+        if (content.length > 0) {
+            count_content--
+            content.length--
+        }
+        res.send("提示<br>(删除成功)")
+    }
+    else if (req.query.type == "award") {
+        for (var i = parseInt(req.query.id); i < count_award - 1; i++)
+            award[i] = award[i + 1]
+        if (award.length > 0) {
+            count_award--
+            award.length--
+        }
+        res.send("提示<br>(删除成功)")
+    }
+    else
+        res.send("警告<br>(删除出错)")
+})
+
+// 监听端口启动服务
+app.listen(8000, () => {
+    console.log("服务启动，8000端口监听ing...");
+})
+
+
+//for test
+/*
+    for(var i=0;i<count_award;i++)
+        console.log(content[i])
+*/
